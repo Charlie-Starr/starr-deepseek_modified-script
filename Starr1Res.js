006200e3787231da2bc21f1b48d19d4156ef3279
Charlie-Starr <danielkanyima9@gmail.com>
2025-07-28 14:56:55 +0100

    Update Starr1Res.js

--- Starr1Res.js
+++ Starr1Res.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Starr (User API Key Edition) - Modified
 // @namespace     http://tampermonkey.net/
-// @version       3.8 // Prompt optimized for performance & restored PI detection
+// @version       4.0 // Prompt optimized for performance & restored PI detection
 // @description   Starr: A real human woman. Seductive, witty, naughty, cheeky, flirty. Now powered by your own OpenRouter API Key. Cone ID access, animated UI, notifications & dark mode.
 // @match         *://*/*
 // @downloadURL   https://charlie-starr.github.io/starr-deepseek_modified-script/Starr1Res.js
